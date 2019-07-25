feee17b61eebe075ba3237f4b3ba47f799dabb04
Deepraj Pradhan <deeprajpradhan@live.com>
2019-07-26 04:00:56 +0530

    Part 1 Complete

---
+++ scraper.py
@@ -0,0 +1,56 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def get_converted_price(price):
+
+    stripped_price = price.strip("₹ ,")
+    replaced_price = stripped_price.replace(",", "")
+    find_dot = replaced_price.find(".")
+    to_convert_price = replaced_price[0:find_dot]
+    converted_price = int(to_convert_price)
+
+    return converted_price
+
+
+def extract_url(url):
+
+    if url.find("www.amazon.in") != -1:
+        index = url.find("/dp/")
+        if index != -1:
+            index2 = index + 14
+            url = "https://www.amazon.in" + url[index:index2]
+        else:
+            url = None
+    else:
+        url = None
+    return url
+
+
+def get_product_details(url):
+
+    headers = {
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 "
+                      "Safari/537.36 "
+    }
+    details = {"name": "", "price": 0, "deal": True, "url": ""}
+    _url = extract_url(url)
+    if _url is None:
+        details = None
+    else:
+        page = requests.get(url, headers=headers)
+        soup = BeautifulSoup(page.content, "html.parser")
+        title = soup.find(id="productTitle")
+        price = soup.find(id="priceblock_dealprice")
+        if price is None:
+            price = soup.find(id="priceblock_ourprice")
+            details["deal"] = False
+        if title is not None and price is not None:
+            details["name"] = title.get_text().strip()
+            details["price"] = get_converted_price(price.get_text())
+            details["url"] = _url
+        else:
+            details = None
+    return details
+
+print(get_product_details("https://www.amazon.in/Test-Exclusive-521/dp/B077Q42J32/ref=br_msw_pdt-2?_encoding=UTF8&smid=A14CZOWI0VEHLG&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=&pf_rd_r=RSHQJHF4YH8ZQJJRNXH3&pf_rd_t=36701&pf_rd_p=9806b2c4-09c8-4373-b954-bae25b7ea046&pf_rd_i=desktop"))
