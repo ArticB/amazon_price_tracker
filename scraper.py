768d7b85ec6da1152de423fa8a6550d1b768c439
Deepraj Pradhan <deeprajpradhan@live.com>
2019-08-02 21:39:34 +0530

    Added: code to extract from /gp/, regex to find price

--- scraper.py
+++ scraper.py
@@ -1,15 +1,16 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 
 
 def get_converted_price(price):
 
-    stripped_price = price.strip("₹ ,")
-    replaced_price = stripped_price.replace(",", "")
-    find_dot = replaced_price.find(".")
-    to_convert_price = replaced_price[0:find_dot]
-    converted_price = int(to_convert_price)
-
+    # stripped_price = price.strip("₹ ,")
+    # replaced_price = stripped_price.replace(",", "")
+    # find_dot = replaced_price.find(".")
+    # to_convert_price = replaced_price[0:find_dot]
+    # converted_price = int(to_convert_price)
+    converted_price = float(re.sub(r"[^\d.]", "", price)) # Thanks to https://medium.com/@oorjahalt
     return converted_price
 
 
@@ -21,7 +22,12 @@ def extract_url(url):
             index2 = index + 14
             url = "https://www.amazon.in" + url[index:index2]
         else:
-            url = None
+            index = url.find("/gp/")
+            if index != -1:
+                index2 = index + 22
+                url = "https://www.amazon.in" + url[index:index2]
+            else:
+                url = None
     else:
         url = None
     return url
@@ -53,4 +59,5 @@ def get_product_details(url):
             details = None
     return details
 
-print(get_product_details("https://www.amazon.in/Test-Exclusive-521/dp/B077Q42J32/ref=br_msw_pdt-2?_encoding=UTF8&smid=A14CZOWI0VEHLG&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=&pf_rd_r=RSHQJHF4YH8ZQJJRNXH3&pf_rd_t=36701&pf_rd_p=9806b2c4-09c8-4373-b954-bae25b7ea046&pf_rd_i=desktop"))
+#print(get_product_details("https://www.amazon.in/Test-Exclusive-521/dp/B077Q42J32/ref=br_msw_pdt-2?_encoding=UTF8&smid=A14CZOWI0VEHLG&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=&pf_rd_r=RSHQJHF4YH8ZQJJRNXH3&pf_rd_t=36701&pf_rd_p=9806b2c4-09c8-4373-b954-bae25b7ea046&pf_rd_i=desktop"))
+print(get_product_details("https://www.amazon.in/gp/product/B00IJRV2D0?pf_rd_p=f2b20090-067d-415f-953d-b8dcecc9109f&pf_rd_r=VFJ98F93X80YWYQNR3GN"))
