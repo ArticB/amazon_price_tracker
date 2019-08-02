615ac3d94a9df16a2045ae7a503282c87dcce666
Deepraj Pradhan <deeprajpradhan@live.com>
2019-08-02 22:24:32 +0530

    bug fixes

--- scraper.py
+++ scraper.py
@@ -45,7 +45,7 @@ def get_product_details(url):
         details = None
     else:
         page = requests.get(url, headers=headers)
-        soup = BeautifulSoup(page.content, "html.parser")
+        soup = BeautifulSoup(page.content, "html5lib")
         title = soup.find(id="productTitle")
         price = soup.find(id="priceblock_dealprice")
         if price is None:
@@ -61,3 +61,4 @@ def get_product_details(url):
 
 #print(get_product_details("https://www.amazon.in/Test-Exclusive-521/dp/B077Q42J32/ref=br_msw_pdt-2?_encoding=UTF8&smid=A14CZOWI0VEHLG&pf_rd_m=A1VBAL9TL5WCBF&pf_rd_s=&pf_rd_r=RSHQJHF4YH8ZQJJRNXH3&pf_rd_t=36701&pf_rd_p=9806b2c4-09c8-4373-b954-bae25b7ea046&pf_rd_i=desktop"))
 print(get_product_details("https://www.amazon.in/gp/product/B00IJRV2D0?pf_rd_p=f2b20090-067d-415f-953d-b8dcecc9109f&pf_rd_r=VFJ98F93X80YWYQNR3GN"))
+#print(get_product_details("https://www.amazon.com/YI-1080P60-Dashboard-G-Sensor-Recording/dp/B01C89GCHU/ref=br_asw_pdt-3?pf_rd_m=ATVPDKIKX0DER&pf_rd_s=&pf_rd_r=ZRGF7W6FRMPM7YHH4ENK&pf_rd_t=36701&pf_rd_p=143861b7-0857-4329-b93d-3eeedb5f1ea9&pf_rd_i=desktop"))
